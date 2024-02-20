a095aa691d2d45730cf0e381d5ed6b5536548acb
AhmadIkbalDjaya <ikbaldjaya@gmail.com>
2024-02-20 13:04:51 +0800

    fix: about ui

--- app/Livewire/Public/About/Index.php
+++ app/Livewire/Public/About/Index.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Public\About;
 
+use App\Models\Profile;
 use App\Models\VillageAparatus;
 use Livewire\Component;
 
@@ -10,6 +11,7 @@ class Index extends Component
     public function render()
     {
         return view('livewire.public.about.index', [
+            "profile" => Profile::get()->first(),
             "village_aparatus" => VillageAparatus::all(),
         ])->title("Tentang Desa");
     }
--- resources/views/livewire/public/about/index.blade.php
+++ resources/views/livewire/public/about/index.blade.php
@@ -98,6 +98,88 @@
   </section>
 
   <section id="welcome" class="font-plusJakartaSans md:mx-40 md:my-20 mx-7 my-8">
+    <h1 class="text-green-700 text-3xl font-bold font-plusJakartaSans text-center pb-3">Informasi Desa</h1>
+    <div class="md:flex md:gap-5 bg-white md:rounded-sm shadow-sm">
+      <div class="md:basis-1/2 pb-0 md:pb-3 p-3 text-lg">
+        <div class="flex my-1">
+          <div class="basis-1/2 text-gray-500">
+            <p class="font-semibold">Nama Desa</p>
+          </div>
+          <div class="basis-1/2">
+            <p class="font-medium">Desa {{ $profile->name }}</p>
+          </div>
+        </div>
+        <div class="flex my-1">
+          <div class="basis-1/2 text-gray-500">
+            <p class="font-semibold">Alamat</p>
+          </div>
+          <div class="basis-1/2">
+            <p class="font-medium">{{ $profile->address }}</p>
+          </div>
+        </div>
+        <div class="flex my-1">
+          <div class="basis-1/2 text-gray-500">
+            <p class="font-semibold">Potensi Desa</p>
+          </div>
+          <div class="basis-1/2">
+            <p class="font-medium">{{ $profile->potency }}</p>
+          </div>
+        </div>
+        <div class="flex my-1">
+          <div class="basis-1/2 text-gray-500">
+            <p class="font-semibold">Status Desa</p>
+          </div>
+          <div class="basis-1/2">
+            <p class="font-medium">{{ $profile->village_status->name }}</p>
+          </div>
+        </div>
+        <div class="flex my-1">
+          <div class="basis-1/2 text-gray-500">
+            <p class="font-semibold">Jumlah Populasi</p>
+          </div>
+          <div class="basis-1/2">
+            <p class="font-medium">{{ $profile->population }} Jiwa</p>
+          </div>
+        </div>
+      </div>
+      <div class="md:basis-1/2 md:mt-3 p-3 pt-0 text-lg">
+        <div class="flex my-1">
+          <div class="basis-1/2 text-gray-500">
+            <p class="font-semibold">Jumlah Dusun</p>
+          </div>
+          <div class="basis-1/2">
+            <p class="font-medium">3 Dusun</p>
+          </div>
+        </div>
+        <div class="flex my-1">
+          <div class="basis-1/2 text-gray-500">
+            <p class="font-semibold">Dusun 1</p>
+          </div>
+          <div class="basis-1/2">
+            <p class="font-medium">Dusun Pariwang</p>
+          </div>
+        </div>
+        <div class="flex my-1">
+          <div class="basis-1/2 text-gray-500">
+            <p class="font-semibold">Dusun 2</p>
+          </div>
+          <div class="basis-1/2">
+            <p class="font-medium">Dusun Bampu</p>
+          </div>
+        </div>
+        <div class="flex my-1">
+          <div class="basis-1/2 text-gray-500">
+            <p class="font-semibold">Dusun 3</p>
+          </div>
+          <div class="basis-1/2">
+            <p class="font-medium">Dusun Ketapi</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="welcome" class="font-plusJakartaSans md:mx-40 md:my-20 mx-7 my-8">
     <h1 class="text-green-700 text-3xl font-bold font-plusJakartaSans text-center pb-3">Lokasi Desa</h1>
     <div class="md:flex md:gap-5 md:bg-white md:rounded-sm shadow-sm">
       <div class="md:basis-1/2">
