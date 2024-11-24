363def0ff569652e77b7dc5a33287bf976c9d778
AhmadIkbalDjaya <ikbaldjaya@gmail.com>
2024-11-24 17:39:53 +0800

    update: public navbar itemsMenu to array and open nav with alpine

--- app/Livewire/Public/Components/Navbar.php
+++ app/Livewire/Public/Components/Navbar.php
@@ -7,16 +7,18 @@ use Livewire\Component;
 
 class Navbar extends Component
 {
-    public $open = false;
+    public $menuItems = [
+        ["label" => "Home", "route" => "home", "active" => "/"],
+        ["label" => "Berita", "route" => "article", "active" => "berita-&-kegiatan*"],
+        ["label" => "Bumdes", "route" => "bumdes", "active" => "bumdes*"],
+        ["label" => "Kelembagaan", "route" => "institution", "active" => "kelembagaan"],
+        ["label" => "Tentang", "route" => "about", "active" => "tentang"],
+    ];
     public function render()
     {
         return view('livewire.public.components.navbar', [
+            "menuItems" => $this->menuItems,
             "profile" => Profile::select(["id", "name"])->first(),
         ]);
-    }
-
-    public function clickOpen()
-    {
-        $this->open = !$this->open;
     }
 }
--- resources/views/livewire/public/components/navbar.blade.php
+++ resources/views/livewire/public/components/navbar.blade.php
@@ -1,70 +1,44 @@
-<div class="fixed left-0 top-0 z-10 w-full bg-white shadow">
+<div x-data="navbar" class="fixed left-0 top-0 z-10 w-full bg-white shadow">
   <header class="px-0 py-0 md:flex md:items-baseline md:gap-x-3 md:px-20 md:py-3">
+    <!-- Logo & Toggle Menu -->
     <div class="flex items-center justify-between px-3 py-3 md:px-0 md:py-0">
       <a wire:navigate href="{{ route('home') }}"
         class="whitespace-nowrap font-justMeAgainDownHere text-3xl font-bold uppercase text-green-700">
         DESA {{ $profile->name }}
       </a>
-      @if ($open)
-        <i wire:click="clickOpen" class="ti ti-menu-2 cursor-pointer text-2xl text-green-700 md:hidden"></i>
-      @else
-        <i wire:click="clickOpen" class="ti ti-menu-deep cursor-pointer text-2xl text-green-700 md:hidden"></i>
-      @endif
+      <i @click="toggle()" :class="open ? 'ti-menu-2' : 'ti-menu-deep'"
+        class="ti ti-menu-2 cursor-pointer text-2xl text-green-700 md:hidden"></i>
     </div>
-    <div
-      class="{{ $open ? 'top-15 opacity-100' : 'top-[-400px] opacity-0 md:opacity-100' }} absolute w-full grow justify-center bg-white px-3 py-1 transition-all duration-500 ease-in md:static md:flex md:items-baseline md:justify-end md:px-0 md:py-0">
+    <!-- Menu Navigasi -->
+    <div :class="open ? 'top-15 opacity-100' : 'top-[-400px] opacity-0 md:opacity-100'"
+      class="absolute w-full grow justify-center bg-white px-3 py-1 transition-all duration-500 ease-in md:static md:flex md:items-baseline md:justify-end md:px-0 md:py-0">
       <ul class="gap-4 md:flex">
-        <li class="my-3 md:my-0">
-          <div class="relative pb-1 text-center">
-            <span
-              class="{{ Request::is('/') ? 'block' : 'hidden' }} absolute inset-x-1/2 bottom-0 h-1 w-8 -translate-x-1/2 rounded-lg bg-green-700 md:inset-x-0 md:w-2/4 md:translate-x-0"></span>
-            <a wire:navigate href="{{ route('home') }}"
-              class="gap-x-1 font-plusJakartaSans text-base font-semibold text-green-700 md:flex">
-              Home
-            </a>
-          </div>
-        </li>
-        <li class="my-3 md:my-0">
-          <div class="relative pb-1 text-center">
-            <span
-              class="{{ Request::is('berita-&-kegiatan*') ? 'block' : 'hidden' }} absolute inset-x-1/2 bottom-0 h-1 w-8 -translate-x-1/2 rounded-lg bg-green-700 md:inset-x-0 md:w-2/4 md:translate-x-0"></span>
-            <a wire:navigate href="{{ route('article') }}"
-              class="gap-x-1 font-plusJakartaSans text-base font-semibold text-green-700 md:flex">
-              Berita
-            </a>
-          </div>
-        </li>
-        <li class="my-3 md:my-0">
-          <div class="relative pb-1 text-center">
-            <span
-              class="{{ Request::is('bumdes*') ? 'block' : 'hidden' }} absolute inset-x-1/2 bottom-0 h-1 w-8 -translate-x-1/2 rounded-lg bg-green-700 md:inset-x-0 md:w-2/4 md:translate-x-0"></span>
-            <a wire:navigate href="{{ route('bumdes') }}"
-              class="gap-x-1 font-plusJakartaSans text-base font-semibold text-green-700 md:flex">
-              Bumdes
-            </a>
-          </div>
-        </li>
-        <li class="my-3 md:my-0">
-          <div class="relative pb-1 text-center">
-            <span
-              class="{{ Request::is('kelembagaan') ? 'block' : 'hidden' }} absolute inset-x-1/2 bottom-0 h-1 w-8 -translate-x-1/2 rounded-lg bg-green-700 md:inset-x-0 md:w-2/4 md:translate-x-0"></span>
-            <a wire:navigate href="{{ route('institution') }}"
-              class="gap-x-1 font-plusJakartaSans text-base font-semibold text-green-700 md:flex">
-              Kelembagaan
-            </a>
-          </div>
-        </li>
-        <li class="my-3 md:my-0">
-          <div class="relative pb-1 text-center">
-            <span
-              class="{{ Request::is('tentang') ? 'block' : 'hidden' }} absolute inset-x-1/2 bottom-0 h-1 w-8 -translate-x-1/2 rounded-lg bg-green-700 md:inset-x-0 md:w-2/4 md:translate-x-0"></span>
-            <a wire:navigate href="{{ route('about') }}"
-              class="gap-x-1 font-plusJakartaSans text-base font-semibold text-green-700 md:flex">
-              Tentang
-            </a>
-          </div>
-        </li>
+        @foreach ($menuItems as $item)
+          <li class="my-3 md:my-0">
+            <div class="relative pb-1 text-center">
+              <span
+                class="{{ Request::is($item['active']) ? 'block' : 'hidden' }} absolute inset-x-1/2 bottom-0 h-1 w-8 -translate-x-1/2 rounded-lg bg-green-700 md:inset-x-0 md:w-2/4 md:translate-x-0"></span>
+              <a wire:navigate href="{{ route($item['route']) }}"
+                class="gap-x-1 font-plusJakartaSans text-base font-semibold text-green-700 md:flex">
+                {{ $item['label'] }}
+              </a>
+            </div>
+          </li>
+        @endforeach
       </ul>
     </div>
   </header>
 </div>
+
+@script
+  <script>
+    Alpine.data('navbar', () => {
+      return {
+        open: false,
+        toggle() {
+          this.open = !this.open;
+        },
+      };
+    });
+  </script>
+@endscript
