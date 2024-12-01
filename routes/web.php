fe6eb828ddc31d452636ea2a561f17567501bf67
AhmadIkbalDjaya <ikbaldjaya@gmail.com>
2024-12-02 06:29:30 +0800

    refactoring: web route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Public as PublicPages;
+use App\Livewire\Admin as AdminPages;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,57 +15,57 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get("", \App\Livewire\Public\Home\Index::class)->name("home");
-Route::get("tentang", \App\Livewire\Public\About\Index::class)->name("about");
-Route::get("bumdes", \App\Livewire\Public\Bumdes\Index::class)->name("bumdes");
-Route::get("bumdes/{bumdes}", \App\Livewire\Public\Bumdes\Show::class)->name("bumdes.show");
-Route::get("kelembagaan", \App\Livewire\Public\Institution\Index::class)->name("institution");
-Route::get("berita-&-kegiatan", \App\Livewire\Public\Article\Index::class)->name("article");
-Route::get("berita-&-kegiatan/{article:slug}", \App\Livewire\Public\Article\Show::class)->name("article.show");
+Route::get("", PublicPages\Home\Index::class)->name("home");
+Route::get("tentang", PublicPages\About\Index::class)->name("about");
+Route::get("bumdes", PublicPages\Bumdes\Index::class)->name("bumdes");
+Route::get("bumdes/{bumdes}", PublicPages\Bumdes\Show::class)->name("bumdes.show");
+Route::get("kelembagaan", PublicPages\Institution\Index::class)->name("institution");
+Route::get("berita-&-kegiatan", PublicPages\Article\Index::class)->name("article");
+Route::get("berita-&-kegiatan/{article:slug}", PublicPages\Article\Show::class)->name("article.show");
 
 Route::middleware(["guest"])->group(function () {
-    Route::get("login", \App\Livewire\Admin\Login::class)->name("login");
+    Route::get("login", AdminPages\Login::class)->name("login");
 });
 
 Route::middleware(["auth"])
     ->prefix("addes")
     ->name("admin.")
     ->group(function () {
-        Route::get("", \App\Livewire\Admin\Index::class)->name("index");
-        Route::get("adminProfile/changePass", \App\Livewire\Admin\AdminProfile\ChangePass::class)->name("admin-profile.changePass");
-        Route::get("population", \App\Livewire\Admin\Poppulation\Index::class)->name("population.index");
-        Route::get("profile", \App\Livewire\Admin\Profile\Index::class)->name("profile.index");
+        Route::get("", AdminPages\Index::class)->name("index");
+        Route::get("adminProfile/changePass", AdminPages\AdminProfile\ChangePass::class)->name("admin-profile.changePass");
+        Route::get("population", AdminPages\Poppulation\Index::class)->name("population.index");
+        Route::get("profile", AdminPages\Profile\Index::class)->name("profile.index");
         Route::prefix("location")->name("location.")->group(function () {
-            Route::get("location", \App\Livewire\Admin\Location\Index::class)->name("index");
+            Route::get("location", AdminPages\Location\Index::class)->name("index");
             Route::prefix('marker')->name("marker.")->group(function () {
-                Route::get("create", \App\Livewire\Admin\Location\Marker\Create::class)->name("create");
-                Route::get("{marker}", \App\Livewire\Admin\Location\Marker\Show::class)->name("show");
-                Route::get("{marker}/edit", \App\Livewire\Admin\Location\Marker\Edit::class)->name("edit");
+                Route::get("create", AdminPages\Location\Marker\Create::class)->name("create");
+                Route::get("{marker}", AdminPages\Location\Marker\Show::class)->name("show");
+                Route::get("{marker}/edit", AdminPages\Location\Marker\Edit::class)->name("edit");
             });
         });
         Route::prefix("bumdes")->name("bumdes.")->group(function () {
-            Route::get("", \App\Livewire\Admin\Bumdes\Index::class)->name("index");
-            Route::get("create", \App\Livewire\Admin\Bumdes\Create::class)->name("create");
-            Route::get("{bumdes}", \App\Livewire\Admin\Bumdes\Show::class)->name("show");
-            Route::get("{bumdes}/edit", \App\Livewire\Admin\Bumdes\Edit::class)->name("edit");
+            Route::get("", AdminPages\Bumdes\Index::class)->name("index");
+            Route::get("create", AdminPages\Bumdes\Create::class)->name("create");
+            Route::get("{bumdes}", AdminPages\Bumdes\Show::class)->name("show");
+            Route::get("{bumdes}/edit", AdminPages\Bumdes\Edit::class)->name("edit");
         });
         Route::prefix("institution")->name("institution.")->group(function () {
-            Route::get("", \App\Livewire\Admin\Institution\Index::class)->name("index");
-            Route::get("create", \App\Livewire\Admin\Institution\Create::class)->name("create");
-            Route::get("{institution}", \App\Livewire\Admin\Institution\Show::class)->name("show");
-            Route::get("{institution}/edit", \App\Livewire\Admin\Institution\Edit::class)->name("edit");
+            Route::get("", AdminPages\Institution\Index::class)->name("index");
+            Route::get("create", AdminPages\Institution\Create::class)->name("create");
+            Route::get("{institution}", AdminPages\Institution\Show::class)->name("show");
+            Route::get("{institution}/edit", AdminPages\Institution\Edit::class)->name("edit");
         });
         Route::prefix("article")->name("article.")->group(function () {
-            Route::get("", \App\Livewire\Admin\Article\Index::class)->name("index");
-            Route::get("create", \App\Livewire\Admin\Article\Create::class)->name("create");
-            Route::get("{article}", \App\Livewire\Admin\Article\Show::class)->name("show");
-            Route::get("{article}/edit", \App\Livewire\Admin\Article\Edit::class)->name("edit");
+            Route::get("", AdminPages\Article\Index::class)->name("index");
+            Route::get("create", AdminPages\Article\Create::class)->name("create");
+            Route::get("{article}", AdminPages\Article\Show::class)->name("show");
+            Route::get("{article}/edit", AdminPages\Article\Edit::class)->name("edit");
         });
         Route::prefix("village-aparatus")->name("village-aparatus.")->group(function () {
-            Route::get("", \App\Livewire\Admin\VillageAparatus\Index::class)->name("index");
-            Route::get("create", \App\Livewire\Admin\VillageAparatus\Create::class)->name("create");
-            Route::get("{village_aparatus}", \App\Livewire\Admin\VillageAparatus\Show::class)->name("show");
-            Route::get("{village_aparatus}/edit", \App\Livewire\Admin\VillageAparatus\Edit::class)->name("edit");
+            Route::get("", AdminPages\VillageAparatus\Index::class)->name("index");
+            Route::get("create", AdminPages\VillageAparatus\Create::class)->name("create");
+            Route::get("{village_aparatus}", AdminPages\VillageAparatus\Show::class)->name("show");
+            Route::get("{village_aparatus}/edit", AdminPages\VillageAparatus\Edit::class)->name("edit");
         });
-        Route::get("complaint", \App\Livewire\Admin\Complaint\Index::class)->name("complaint.index");
+        Route::get("complaint", AdminPages\Complaint\Index::class)->name("complaint.index");
     });
